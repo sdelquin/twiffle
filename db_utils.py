ff240808d26b022876113369d87be75be86e8c3e
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-01-21 12:09:27 +0000

    Add module for database management

---
+++ db_utils.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+
+class DBHandler:
+    def __init__(self, database_filename='twiffle.db'):
+        self.conn = sqlite3.connect(database_filename)
+        self.cursor = self.conn.cursor()
+
+    def init_db(self):
+        query = '''CREATE TABLE status (
+                id TEXT PRIMARY KEY,
+                username TEXT,
+                text TEXT,
+                created_at TIMESTAMP,
+                url TEXT,
+                is_retweet INTEGER);'''
+        try:
+            self.cursor.execute(query)
+        except sqlite3.OperationalError:
+            print('Database already exists!')
+
+    def insert_record(self, id, username, text, created_at, url, is_retweet, /):
+        query = 'INSERT INTO status VALUES (?, ?, ?, ?, ?, ?)'
+        self.cursor.execute(query, (id, username, text, created_at, url, is_retweet))
