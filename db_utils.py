0fa4980d044989129c7c83f0c3ffa13f7ee17f42
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-01-21 16:52:07 +0000

    Add method to extract users from database

--- db_utils.py
+++ db_utils.py
@@ -1,9 +1,13 @@
 import sqlite3
+from datetime import datetime
 
 
 class DBHandler:
     def __init__(self, database_filename='twiffle.db'):
-        self.conn = sqlite3.connect(database_filename)
+        self.conn = sqlite3.connect(
+            database_filename, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+        )
+        self.conn.row_factory = sqlite3.Row
         self.cursor = self.conn.cursor()
 
     def init(self):
@@ -23,3 +27,20 @@ class DBHandler:
         query = 'INSERT INTO status VALUES (?, ?, ?, ?, ?, ?)'
         self.cursor.execute(query, (id, username, text, created_at, url, is_retweet))
         self.conn.commit()
+
+    def extract_users(self, since: str = None, until: str = None):
+        """Extract unique usernames with tweets created after "since" and before "until"
+        Parameters
+        ----------
+            since: str with isoformat "YYYY-MM-DD HH:MM:SS"
+            until: str with isoformat "YYYY-MM-DD HH:MM:SS"
+        """
+        since = since or datetime.min.isoformat(' ', 'seconds')
+        until = until or datetime.now().isoformat(' ', 'seconds')
+        query = '''SELECT DISTINCT(username)
+                   FROM status
+                   WHERE created_at >= ? and created_at <= ?'''
+        self.cursor.execute(query, (since, until))
+        rows = self.cursor.fetchall()
+        for row in rows:
+            print(row['username'])
